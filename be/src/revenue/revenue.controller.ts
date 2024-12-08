8b4a340ebdb85209a88f861f7c7a074ba308b4d0
TH-1711 <trantrunghieu1593572@gmail.com>
2024-12-09 06:44:21 +0700

    Initial Revenue

---
+++ be/src/revenue/revenue.controller.ts
@@ -0,0 +1,14 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Put,
+  Delete,
+  ParseIntPipe,
+} from "@nestjs/common";
+import { RevenueService } from "../revenue/revenue.service";
+
+@Controller("revenue")
+export class RevenueController {
+  constructor(private readonly revenueService: RevenueService) {}
+}
---
+++ be/src/revenue/revenue.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { RevenueService } from "./revenue.service";
+import { RevenueController } from "./revenue.controller";
+import { PrismaService } from "../prisma/prisma.service"; // Make sure the PrismaService is correctly imported
+
+@Module({
+  controllers: [RevenueController],
+  providers: [RevenueService, PrismaService],
+})
+export class RevenueModule {}
